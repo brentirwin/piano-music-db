e270d51a278a7d0d2aa1cc6d67e340b281ab34e1
Brent Irwin <brent@brentirwin.tv>
2019-05-09 12:43:17 -0500

    Create create-tables.sql

---
+++ create-tables.sql
@@ -0,0 +1,54 @@
+CREATE TABLE piece (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  composer_id INT NOT NULL,   -- fk: composer.id
+  title       TEXT NOT NULL,
+  class_id    INT,            -- fk: class.id
+  movement    INT,
+  rcm_grade   INT,
+  rcm_list    CHAR(1),
+  henle_grade INT
+);
+
+CREATE TABLE composer (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name  TEXT,
+  last_name   TEXT NOT NULL,
+  era_id      INT             -- fk: era.id
+);
+
+CREATE TABLE era (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name  TEXT NOT NULL
+);
+
+CREATE TABLE class (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  prefix  TEXT,
+  work    INT,
+  no      INT,
+  coll_id INT                 -- fk: collection.id
+);
+
+CREATE TABLE collection (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name  TEXT NOT NULL
+);
+
+CREATE TABLE book (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title       TEXT NOT NULL,
+  composer_id INT,            -- fk: composer.id 
+  pub_id      INT,            -- fk: publisher.id
+  own         INT -- bool
+);
+
+CREATE TABLE publisher (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name  TEXT NOT NULL
+);
+
+CREATE TABLE book_piece (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  b_id  INT NOT NULL,         -- fk: book.id
+  p_id  INT NOT NULL          -- fk: piece.id
+);
